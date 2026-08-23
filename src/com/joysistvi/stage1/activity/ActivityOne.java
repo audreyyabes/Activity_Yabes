1bd3d56483c2b6186d46c31f37a3ba2bd959dec0
audreyyabes <audreyyabes28@gmail.com>
2026-08-24 01:41:20 +0800

    Initial commit

---
+++ src/com/joysistvi/stage1/activity/ActivityOne.java
@@ -0,0 +1,25 @@
+package com.joysistvi.stage1.activity;
+
+public class ActivityOne {
+    public static void main(String[] args) {
+        String fname = "Audrey";
+        byte currentTemperature = -37;
+        short shortNumber = 300;
+        int age = 26;
+        long longNumber = 25000;
+        float floatNumber = 23.27F;
+        double doubleNumber = 27.58;
+        char middleName = 'M';
+        boolean trueFalse = false;
+
+        System.out.println("First name " + fname);
+        System.out.println("Middle Initial " + middleName);
+        System.out.println("Age " + age);
+        System.out.println("Current Temperature " + currentTemperature);
+        System.out.println("Short Number " + shortNumber);
+        System.out.println("Long Number " + longNumber);
+        System.out.println("Float number " + floatNumber);
+        System.out.println("Double Number" + doubleNumber);
+        System.out.println("True or False: " + trueFalse);
+    }
+}
---
+++ src/com/joysistvi/stage1/activity/ActivityTwo_Yabes.java
@@ -0,0 +1,4 @@
+package com.joysistvi.stage1.activity;
+
+public class ActivityTwo_Yabes {
+}
